556c4b958c42785bcb5dd34b6492750dfc5218f7
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-01-07 23:50:59 +0530

    theme_chager file making

--- theme_changer/src/main.jsx
+++ theme_changer/src/main.jsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
+  <StrictMode>
     <App />
-</ThemeProvider>
-
+  </StrictMode>,
 )
