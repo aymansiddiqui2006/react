762441394ccac2904f66412c4e2822d37887f73f
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-01-17 17:25:28 +0530

    started todo list

---
+++ to-do list/src/context/To-doContext.js
@@ -0,0 +1,21 @@
+import { createContext,useContext } from "react";
+
+export const To_doContext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:"todo msg",
+            completed:false,
+        },
+       ],
+       addTodo:(todo)=>{},
+       updateTodo:(id,todo)=>{},
+       deleteTodo:(id)=>{},
+       tooglecomplete:(id)=>{},
+})
+
+export const useToDo=()=>{
+    return useContext(To_doContext)
+}
+
+export const todoProvider=To_doContext.Provider
