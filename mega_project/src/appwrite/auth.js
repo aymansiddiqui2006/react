2dd3a35b9d9f168e5bef1dfa976ff3f2888032e7
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-01-25 20:55:47 +0530

    auth in mega project

---
+++ mega_project/src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import conf from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteurl)
+            .setProject(conf.appwriteprojectid);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+
+            if (userAccount) {
+                // auto login after signup
+                return this.login({ email, password });
+            }
+
+            return userAccount;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(
+                email,
+                password
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            return null;
+        }
+    }
+}
+
+const authService = new AuthService();
+export default authService;
