8645ff79137d45a9a974a42a21a1107fc4d06ec5
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-01-25 15:00:58 +0530

    mega project initial file making from vite

---
+++ rudux/src/app/feature/todo/todoSlice.js
@@ -0,0 +1,53 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    { id: 1, text: "hello world", completed: false }
+  ],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        id: nanoid(),
+        text: action.payload,
+        completed: false,
+      });
+    },
+
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter(
+        (todo) => todo.id !== action.payload
+      );
+    },
+
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((t) => t.id === id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
+
+    toggleComplete: (state, action) => {
+      const todo = state.todos.find(
+        (todo) => todo.id === action.payload
+      );
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
+
+export const {
+  addTodo,
+  removeTodo,
+  updateTodo,
+  toggleComplete,
+} = todoSlice.actions;
+
+export default todoSlice.reducer;
