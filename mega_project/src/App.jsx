d6d4cf2d98662e6f467b5559baf8246c02dc7475
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-01-25 23:59:47 +0530

    header and footer and chnages

--- mega_project/src/App.jsx
+++ mega_project/src/App.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './config/store/authSlice'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setload] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login(userData))
+      } else {
+        dispatch(logout())
+      }
 
-  return (
-    <h1>hell0</h1>
-  )
+    }).finally(() => {
+      setload(false)
+    })
+  }, [])
+
+  return !loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-600'>
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
+  ):null
+  
 }
 
 export default App
